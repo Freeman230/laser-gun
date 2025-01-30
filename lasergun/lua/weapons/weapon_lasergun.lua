8b160f5b3a0b4f8ebaa0c1cebb6334fae8b73652
1999 <155086530+Freeman230@users.noreply.github.com>
2025-01-30 20:09:04 +0300

    Update weapon_lasergun.lua

--- lasergun/lua/weapons/weapon_lasergun.lua
+++ lasergun/lua/weapons/weapon_lasergun.lua
@@ -1,4 +1,3 @@
-local p = FindMetaTable("Player")
 local e = FindMetaTable("Entity")
 
 SWEP.Author = "1999"
@@ -29,49 +28,122 @@ SWEP.Primary.Automatic = false
 SWEP.Primary.Ammo		= ""
 
 SWEP.AutoSwitchTo = true
-SWEP.AutoSwitchFrom = true
+SWEP.AutoSwitchFrom = true   
 
-local function grm(ent)
-	if !SERVER then return '' end
+local function LaserAttack1(e,self)
 
-	return ent:GetInternalVariable('model')
+local laser = {}
+
+laser.Callback = function(a, tr, d)
+
+    tr.Entity:SetHealth(0)
+	tr.Entity:Dissolve(math.random(0,3))
+	tr.Entity:Ignite(5) 
+	
+	tr.Entity.AcceptInput = function() return false end                    
+    tr.Entity.OnRemove = function(self,...) self:Remove() end
+	tr.Entity.OnDeath = function(self,...) self:Remove() end
+	tr.Entity.OnTakeDamage = function(self,...) self:Remove() end
+	tr.Entity.OnTraceAttack = function(self,...) self:Remove() end
+	tr.Entity.CustomThink = function(self,...) self:Remove() end
+	tr.Entity.Think = function(self,...) self:Remove() end
+	
+	
+	if (tr.Entity:IsPlayer() and tr.Entity:HasGodMode()) then
+	     tr.Entity:Kill()
+	     tr.Entity:Dissolve(math.random(0,3))
+	     tr.Entity:RemoveFlags(32768)
+    end
+	
+	if (tr.Entity:IsPlayer() and tr.Entity:IsAlive()) then
+	     tr.Entity:Kill()
+	     tr.Entity:Dissolve(math.random(0,3))
+	     tr.Entity:RemoveFlags(32768)
+    end
+	
+    d:SetDamage(1/0)						  
+    d:SetInflictor(self.Owner)
+    d:SetAttacker(self.Owner)
+    d:SetDamageType(DMG_AIRBOAT,DMG_BLAST,DMG_NEVERGIB,DMG_DIRECT,DMG_ENERGYBEAM)
+    d:SetDamageForce(self.Owner:GetAimVector()*1e9)
+	
 end
 
-local function CreateEntityRagdoll(ent, ply, skin, force)
+    laser.Num = 1
+    laser.Src = self.Owner:GetShootPos()			
+    laser.Dir = self.Owner:GetAimVector()
+    laser.Force = 1/0
+    laser.Damage = 1/0
+    laser.Trace = 1
+    laser.TracerName 	= "ToolTracer"
+	laser.Attacker = self.Owner
 
-    if ( not !IsValid(ent) ) then return end
-    
-	local model = grm(ent)
-	local clr = Color(ent:GetColor().r, ent:GetColor().g, ent:GetColor().b)
-    if SERVER and (model and util.IsValidRagdoll(model)) then
-        local ragdoll = ents.Create("prop_ragdoll")
-        ragdoll:SetModel(model)
-        ragdoll:SetSkin(skin or 0)
-        ragdoll:SetPos(ent:GetPos())
-        ragdoll:SetAngles(ent:GetAngles())
-		ragdoll:SetColor(clr)
-		ragdoll:SetMaterial(ent:GetMaterial())
-        ragdoll:Spawn()
+    self:FireBullets(laser)
+	
+	if SERVER then
+	
+	local hitscan = ents.FindAlongRay(self.Owner:GetShootPos() + self.Owner:GetAimVector(), self.Owner:GetEyeTrace().HitPos)
+	
+    for _, e in pairs(hitscan) do
+		
+		 if e~=self.Owner then
+		
+		     if (e:IsPlayer() and e:HasGodMode()) then
+	              e:Kill()
+	              e:Dissolve(math.random(0,3))
+	              e:RemoveFlags(32768)
+             end
+	
+             if (e:IsPlayer() and e:IsAlive()) then
+	              e:Kill()
+	              e:Dissolve(math.random(0,3))
+	              e:RemoveFlags(32768)
+             end
+			
+		      if e:GetClass()~="predicted_viewmodel" and not(e:IsWeapon() and e:GetOwner()==self.Owner) and e:GetClass()~="gmod_hands" and e:IsValid() then
 
-		if IsValid(ply) then
-			AddUndoEntity(ply, ragdoll, ClassName(ent))
+			         for i = 1, 4 do
+					      e:EmitSound("weapons/fx/rics/ric1.wav")
+					 end
+			
+               local d = DamageInfo()
+                     d:SetDamage(1/0)						  
+                     d:SetInflictor(self.Owner)
+                     d:SetAttacker(self.Owner)
+                     d:SetDamageType(DMG_AIRBOAT,DMG_BLAST,DMG_NEVERGIB,DMG_DIRECT,DMG_ENERGYBEAM)
+                     d:SetDamageForce(self.Owner:GetAimVector()*1e9)
+                     e:TakeDamageInfo(d)
+					 
+					 e.AcceptInput = function() return false end                    
+			         e.OnRemove = function(self,...) self:Remove() end
+					 e.OnDeath = function(self,...) self:Remove() end
+					 e.OnTakeDamage = function(self,...) self:Remove() end
+					 e.OnTraceAttack = function(self,...) self:Remove() end
+			         e.CustomThink = function(self,...) self:Remove() end
+			         e.Think = function(self,...) self:Remove() end
+	
+					 e:SetHealth(0)
+					 e:Dissolve(math.random(0,3))
+		             e:Ignite(5)	
+					 e:SetVelocity(self.Owner:GetAimVector()*10000)
+					 
+					if e:GetClass()=="prop_ragdoll" then
+					    timer.Create(tostring(e), 0.15, 10 * 17, function()
+                            if e:IsValid() then
+                                for i = 1, e:GetPhysicsObjectCount() - 1 do
+                                local phys = e:GetPhysicsObjectNum(i)
+                                    if phys:IsValid() then
+                                    local randomVelocity = Vector(math.random(-7, 7), math.random(-7, 7), math.random(-7, 7)) * math.random(50, 300)
+                                    phys:SetVelocity(randomVelocity)
+                                    end
+                                end
+                            end
+                        end)
+					end	
+				end				
+			end	 					
 		end
-    
-        for i = 0, ragdoll:GetPhysicsObjectCount()-1 do
-            local bone = ragdoll:GetPhysicsObjectNum(i)
-            local pos, ang = ent:GetBonePosition(ragdoll:TranslatePhysBoneToBone(i))
-            if bone and pos and ang then
-                bone:SetAngles(ang)
-                bone:SetPos(pos)
-            end
-
-			if force then
-				bone:SetVelocity(force)
-			end
-        end
-
-		ent.CorpseRag = true
-    end
+	end
 end
 
 function SWEP:FireAnimationEvent(pos,ang,event,options)
@@ -84,86 +156,10 @@ function SWEP:Initialize()
 end
 
 function SWEP:PrimaryAttack()
-
-self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
-self.Owner:SetAnimation(PLAYER_ATTACK1)
-self:SetNextPrimaryFire(CurTime()+0.01)
-self.Weapon:EmitSound(Sound(self.Primary.Sound))
-
-local laser = {}
-
-laser.Callback = function(a, tr, d)
-  
-if SERVER and (tr.Entity:IsPlayer() and tr.Entity:HasGodMode()) then
-    tr.Entity:Kill()
-end
-  
-if (tr.Entity:Health()>0) and IsValid(tr.Entity) then
-    tr.Entity:Fire("Kill")
-end
-
-    d:SetAttacker(self.Owner)
-    d:SetInflictor(self.Owner)
-    d:SetDamage(1/0)
-	d:SetDamageForce(self.Owner:GetAimVector()*1e9)
-    d:SetDamageType(bit.bor(DMG_AIRBOAT,DMG_BLAST,DMG_NEVERGIB,DMG_DIRECT,DMG_ENERGYBEAM))
-    tr.Entity:TakeDamageInfo(d)
-	
-	if SERVER then
-	local hitscan = ents.FindAlongRay(self:GetOwner():GetShootPos() + self:GetOwner():GetAimVector(), self:GetOwner():GetEyeTrace().HitPos)
-	    for _, e in pairs(hitscan) do
-		
-		if e~=self.Owner then
-	       
-		      if e:GetClass()~="predicted_viewmodel" and not(e:IsWeapon() and e:GetOwner()==self.Owner) and e:GetClass()~="gmod_hands" and e:IsValid() then
-			  
-			  local dmginfo = DamageInfo()
-					dmginfo:SetDamage(1/0)
-					dmginfo:SetInflictor(self.Owner)
-					dmginfo:SetAttacker(self.Owner)
-					dmginfo:SetDamageType(DMG_AIRBOAT,DMG_BLAST,DMG_NEVERGIB,DMG_DIRECT,DMG_ENERGYBEAM,DMG_BURN)
-					dmginfo:SetDamageForce(self.Owner:GetAimVector()*1e9)
-					
-					e:TakeDamageInfo(dmginfo)
-			        e:SetHealth(0)
-					e:NextThink(CurTime() + 5 )
-					CreateEntityRagdoll(ent, ply, skin, force)
-					
-					    for i = 1, 4 do
-					        e:EmitSound("weapons/fx/rics/ric1.wav")
-					    end
-					
-					e.AcceptInput = function() return false end                    
-			        e.OnRemove = function(e,...) e:Remove() end
-					e.OnDeath = function(e,...) e:Remove() end
-					e.OnTakeDamage = function(e,...) e:Remove() end
-			        e.CustomThink = function(e,...) e:Remove() return end
-			        e.Think = function(e,...) e:Remove() return end
-					
-					if e and !IsValid(e) then
-					   e:Fire("Kill", "", 0.01)		
-                    end	
-
-                    if e:GetClass()=="npc_combinedropship" or e:GetClass()=="npc_combinegunship" then -- These two don't get killed properly so we remove them
-					    e:Remove()
-                    end	
-							 					
-		       end
-	         end
-            end
-      end
-end
-
-    laser.Num = 1
-    laser.Src = self.Owner:GetShootPos()			
-    laser.Dir = self.Owner:GetAimVector()
-    laser.Force = 1/0
-    laser.Damage = 1/0
-    laser.Trace = 1
-    laser.TracerName 	= "ToolTracer"
-
-    self:FireBullets(laser)
- 
+    self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+    self.Owner:SetAnimation(PLAYER_ATTACK1)
+    self.Weapon:EmitSound(Sound(self.Primary.Sound))
+    LaserAttack1(e,self)
 end
 
 function SWEP:SecondaryAttack()
